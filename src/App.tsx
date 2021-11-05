c94bbe04fe0ab5d98553d89bb641daefc1700f5b
ManuelMares <30966062+ManuelMares@users.noreply.github.com>
2021-11-05 10:39:43 -0600

    Implementation of React redux and routed got fixed by allowing the re… (#10)
    * Implementation of React redux and routed got fixed by allowing the refresh of interface between link and the reduced variables
    
    * Update src/config/Interfaces/page.ts
    
    Co-authored-by: Gerardo Mares Solano <gerardo0579@hotmail.com>
    
    Co-authored-by: ManuelMares <manuelmares@LAPTOP-LMLV627R.localdomain>
    Co-authored-by: Gerardo Mares Solano <gerardo0579@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,32 @@ import {Dashboard} from './Components/Dashboard/Dashboard'
 import {Menu} from './Components/Menu/Menu'
 import {NotFoundPage} from './Components/NotFoundPage/NotFoundPage'
 import {Catalogo} from './Components/Catalogo/Catalogo'
-import {BrowserRouter, Switch, Route, Redirect, withRouter } from "react-router-dom";
+import {BrowserRouter, Switch, Route, RouteComponentProps, Redirect, withRouter } from "react-router-dom";
 import {Provider} from 'react-redux'
+import routes from './config/routes'; 
 
 export const App:React.FC = () =>  {
   
   return (  
-    <BrowserRouter> 
+    <BrowserRouter>
       <Menu/> 
       <Switch>
-          <Route exact path="/Dashboard" component={Dashboard}  />
-          <Route path="/Catalogo" component={Catalogo}  />
-          <Route path="/IniciarSesion" component={IniciarSesion}  />
-          <Redirect from="/" to="/Dashboard" />
-          <Route path="*" component={NotFoundPage}  />
+          {routes.map((route, index) => {
+            return(
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                render={(props: RouteComponentProps<any>) => (
+                  <route.component
+                    name = {route.name}
+                    {...props}
+                    {...route.props}
+                  />
+                )}
+              />
+            )
+          })}
       </Switch>  
     </BrowserRouter>
   );
--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Box, Text, VStack } from "@chakra-ui/react";
 import NewPacient from "../Form/FormTemplates/NewPacient";
+import { RouteComponentProps, withRouter } from "react-router-dom";
+import IPage from "../../config/Interfaces/page";
 
-export const Dashboard: React.FC = () => {
+export const Dashboard: React.FC<IPage & RouteComponentProps> = () => {
   return (
     <VStack>
       <Text data-testid="DashboardTitulo">Hola. Este es el dashboard</Text>
